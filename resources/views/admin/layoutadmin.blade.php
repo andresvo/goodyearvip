391c4e6ed8ee10337dcee2a5d61637e74256180a
Andrés V <andres@valsis.cl>
2020-07-28 13:09:33 -0400

    Ventas: quita link excel cuando no hay ventas. Menú: active.

--- resources/views/admin/layoutadmin.blade.php
+++ resources/views/admin/layoutadmin.blade.php
@@ -4,7 +4,7 @@
 	<title>Goodyear VIP</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=0.8">
-	<link rel="stylesheet"  href="{{ asset('principal.css?v=4') }}" type="text/css" media="all" />
+	<link rel="stylesheet"  href="{{ asset('principal.css?v=5') }}" type="text/css" media="all" />
 	<script type="text/javascript" src="{{ asset('jquery-1.11.1.min.js') }}"></script>
 </head>
 <body>
@@ -16,11 +16,11 @@
 	<h2>Administrador</h2>
 
 	<nav>
-		<a href="{{ URL::to('admin') }}">Ventas</a>
-		<a href="{{ URL::to('admin/tarjetas') }}">Tarjetas</a>
-		<a href="{{ URL::to('admin/disenos') }}">Diseños Tarjetas</a>
-		<a href="{{ URL::to('admin/concurso') }}">Concurso</a>
-		<a href="{{ URL::to('admin/productos') }}">Productos</a>
+		<a class="{{ (substr(url()->current(), -5) == 'admin')? 'active' : '' }}" href="{{ URL::to('admin') }}">Ventas</a>
+		<a class="{{ (strstr(url()->current(), 'tarjetas') !== false)? 'active' : '' }}" href="{{ URL::to('admin/tarjetas') }}">Tarjetas</a>
+		<a class="{{ (strstr(url()->current(), 'disenos') !== false)? 'active' : '' }}" href="{{ URL::to('admin/disenos') }}">Diseños Tarjetas</a>
+		<a class="{{ (strstr(url()->current(), 'concurso') !== false)? 'active' : '' }}" href="{{ URL::to('admin/concurso') }}">Concurso</a>
+		<a class="{{ (strstr(url()->current(), 'productos') !== false)? 'active' : '' }}" href="{{ URL::to('admin/productos') }}">Productos</a>
 	</nav>
 	@yield('content')
 </div>
