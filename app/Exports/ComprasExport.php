1afdfff81e7cc8dab23ee832033ab28a80148e4b
Andrés V <andres@valsis.cl>
2021-10-28 21:35:38 -0300

    ComprasExport: agrega columna 12%.

--- app/Exports/ComprasExport.php
+++ app/Exports/ComprasExport.php
@@ -22,7 +22,7 @@ class ComprasExport implements FromArray, WithHeadings, ShouldAutoSize, WithEven
 		$compras = DB::select( DB::raw("SELECT usuario.email, medida.nombre AS mnombre, producto.nombre AS pnombre, cantidad, tarjeta.codigo, compra.created_at, precio, boleta, factura, medida.sku FROM compra JOIN usuario ON compra.id_usuario = usuario.id JOIN medida ON compra.id_medida = medida.id JOIN producto ON medida.id_producto = producto.id JOIN tarjeta ON compra.id_tarjeta = tarjeta.id $where ORDER BY compra.id DESC") );
 		
 		foreach($compras as $c) {
-			$sheet[] = [$c->email, $c->pnombre, $c->mnombre, $c->cantidad, $c->codigo, $c->precio, $c->boleta, $c->factura, $c->created_at, $c->sku];
+			$sheet[] = [$c->email, $c->pnombre, $c->mnombre, $c->cantidad, $c->codigo, $c->precio, $c->boleta, $c->factura, $c->created_at, $c->sku, round($c->precio * 0.12)];
 		}
 
 		return $sheet;
@@ -30,14 +30,14 @@ class ComprasExport implements FromArray, WithHeadings, ShouldAutoSize, WithEven
 	
 	public function headings(): array
     {
-        return ['Usuario', 'Diseño', 'Medida', 'Cantidad', 'Tarjeta', 'Precio unitario (precio a público vigente este mes)', 'Boleta', 'Factura', 'Fecha creación', 'SKU Goodyear'];
+        return ['Usuario', 'Diseño', 'Medida', 'Cantidad', 'Tarjeta', 'Precio unitario (precio a público vigente este mes)', 'Boleta', 'Factura', 'Fecha creación', 'SKU Goodyear', '12% de apoyo'];
 	}
 	
 	public function registerEvents(): array
     {
         return [
             AfterSheet::class    => function(AfterSheet $event) {
-                $cellRange = 'A1:I1'; // All headers
+                $cellRange = 'A1:K1'; // All headers
                 $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setBold(true);
             },
         ];
