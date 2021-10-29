bc9d153eb265877dc33c77f84c1ce41ac3fad783
Andrés V <andres@valsis.cl>
2021-10-28 21:29:50 -0300

    ComprasExport: agrega sku. Formulario serviteca: actualiza select y placeholder.

--- app/Exports/ComprasExport.php
+++ app/Exports/ComprasExport.php
@@ -19,10 +19,10 @@ class ComprasExport implements FromArray, WithHeadings, ShouldAutoSize, WithEven
 		$where = '';
 		if($this->id_empresa != null) $where = ' WHERE tarjeta.id_empresa = ' . intval($this->id_empresa);
 
-		$compras = DB::select( DB::raw("SELECT usuario.email, medida.nombre AS mnombre, producto.nombre AS pnombre, cantidad, tarjeta.codigo, compra.created_at, precio, boleta, factura FROM compra JOIN usuario ON compra.id_usuario = usuario.id JOIN medida ON compra.id_medida = medida.id JOIN producto ON medida.id_producto = producto.id JOIN tarjeta ON compra.id_tarjeta = tarjeta.id $where ORDER BY compra.id DESC") );
+		$compras = DB::select( DB::raw("SELECT usuario.email, medida.nombre AS mnombre, producto.nombre AS pnombre, cantidad, tarjeta.codigo, compra.created_at, precio, boleta, factura, medida.sku FROM compra JOIN usuario ON compra.id_usuario = usuario.id JOIN medida ON compra.id_medida = medida.id JOIN producto ON medida.id_producto = producto.id JOIN tarjeta ON compra.id_tarjeta = tarjeta.id $where ORDER BY compra.id DESC") );
 		
 		foreach($compras as $c) {
-			$sheet[] = [$c->email, $c->pnombre, $c->mnombre, $c->cantidad, $c->codigo, $c->precio, $c->boleta, $c->factura, $c->created_at];
+			$sheet[] = [$c->email, $c->pnombre, $c->mnombre, $c->cantidad, $c->codigo, $c->precio, $c->boleta, $c->factura, $c->created_at, $c->sku];
 		}
 
 		return $sheet;
@@ -30,7 +30,7 @@ class ComprasExport implements FromArray, WithHeadings, ShouldAutoSize, WithEven
 	
 	public function headings(): array
     {
-        return ['Usuario', 'Diseño', 'Medida', 'Cantidad', 'Tarjeta', 'Precio unitario', 'Boleta', 'Factura', 'Fecha creación'];
+        return ['Usuario', 'Diseño', 'Medida', 'Cantidad', 'Tarjeta', 'Precio unitario (precio a público vigente este mes)', 'Boleta', 'Factura', 'Fecha creación', 'SKU Goodyear'];
 	}
 	
 	public function registerEvents(): array
