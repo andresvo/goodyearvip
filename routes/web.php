d00e70b4e611f45e0af5c707a2b5ed50d8c7c54e
Andrés V <andres@valsis.cl>
2023-07-06 20:58:51 -0400

    Revert "Actualiza bases."
    This reverts commit 18f5c720ed821b25a78e1c710d9f669d0d1fe92d.

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ Route::get('como-funciona', function()
 
 Route::get('bases', function()
 {
-    return redirect('descargas/BASES PROGRAMA CLIENTE VIP 2023 (002).docx');
+    return redirect('descargas/BASES PROGRAMA CLIENTE VIPv3.2023.06.01.docx.pdf');
 });
 
 Route::get('contacto', function()
